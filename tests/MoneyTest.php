dea43e129af3a744653e8d37d88de43dab97f241
Patrick <privat@skyraptor.eu>
2022-02-06 19:10:06 +0100

    Update MoneyTest.php

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -11,8 +11,6 @@ class MoneyTest extends TestCase
     /** @test */
     public function i_am_not_insane()
     {
-        $cart = $this->getCart();
-
         $price = new Money(1000, new Currency('USD'));
 
         $this->assertEquals(new Money(210, new Currency('USD')), $price->multiply(0.21));
